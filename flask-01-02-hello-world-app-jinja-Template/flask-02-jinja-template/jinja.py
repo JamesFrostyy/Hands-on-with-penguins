50707794aa46adc4d51b0081e6490b6059bbe532
Cem AYAZ (James F.) <108179313+JamesFrostyy@users.noreply.github.com>
2022-08-15 16:20:10 +0300

    jinya.py completed

--- flask-01-02-hello-world-app-jinja-Template/flask-02-jinja-template/jinja.py
+++ flask-01-02-hello-world-app-jinja-Template/flask-02-jinja-template/jinja.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World from Flask!!!'
-    
-@app.route('/second')
-def second():
-    return 'Group Penguins!!!'
+@app.route("/")
+def head():
+    return render_template("index.html", number1=34, number2=45)
+
+
+@app.route("/cem")
+def number():
+    num1 = 23
+    num2 = 54
+    return render_template("body.html", num1=num1, num2=num2, sum=num1+num2)
 
 
 if __name__== "__main__":
