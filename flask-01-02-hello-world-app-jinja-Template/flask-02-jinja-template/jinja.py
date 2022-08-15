10a030353e3b6ef5129ec4ba635dab1498d8395f
Cem AYAZ (James F.) <108179313+JamesFrostyy@users.noreply.github.com>
2022-08-15 15:53:31 +0300

    Part1 and Part2 completed

--- flask-01-02-hello-world-app-jinja-Template/flask-02-jinja-template/jinja.py
+++ flask-01-02-hello-world-app-jinja-Template/flask-02-jinja-template/jinja.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return 'Hello World from Flask!!!'
+    
+@app.route('/second')
+def second():
+    return 'Group Penguins!!!'
+
 
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=2000)
